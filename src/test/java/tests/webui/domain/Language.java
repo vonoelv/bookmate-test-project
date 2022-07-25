73fec3066ffb849c29a662d6a978c110c37ae654
Vyacheslav Leonov <vyach.son@gmail.com>
2022-07-26 01:28:06 +0300

    fix base url issue

--- src/test/java/tests/webui/domain/Language.java
+++ src/test/java/tests/webui/domain/Language.java
@@ -3,7 +3,7 @@ package tests.webui.domain;
 import config.Project;
 
 public enum Language {
-    ENGLISH("English", Project.config.baseUrl()),
+    ENGLISH("English", Project.config.baseUrl() + "/"),
     RUSSIAN("Русский", Project.config.baseUrl().replace("//", "//ru.") + "/"),
     SPANISH("Español", Project.config.baseUrl().replace("//", "//es.") + "/"),
     DUTCH("Dansk", Project.config.baseUrl().replace("//", "//da.") + "/"),
